e231f522f7406fc57e3c551f4ff41af21c602d97
EscapedSheep <sfgmdf001@126.com>
2020-09-10 20:35:20 +0800

    [maida] feat: pass all test

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,12 @@
-const parseData
+const parseData = ({data, column}) => {
+    let parseResult = [];
+    for (const row of data) {
+        let dataEntity = {};
+        for (let i = 0; i < column.length; i++) {
+            dataEntity[column[i].name] = row[i];
+        }
+        parseResult.push(dataEntity);
+    }
+    return parseResult;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,20 @@
-const inject
+const inject = (items, sections) => {
+    let currentIndex = 0;
+    let result = [];
+    items.reverse();
+    for (const section of sections) {
+        while(currentIndex !== section.index) {
+            result.push(items.pop());
+            currentIndex += 1;
+        }
+        result.push(section.content);
+
+    }
+
+    const leftCount = items.length;
+    for (let i = 0; i < leftCount; i++) {
+        result.push(items.pop());
+    }
+    return result;
+};
 export { inject };
